22fb6c7da18a6bed377effee90e439d4d47a2332
f4263 <fulvio.russo@guest.italiaonline.it>
2018-09-28 18:46:38 +0200

    refactoring

--- src/test/java/salesTaxes/TaxCalculatorTest.java
+++ src/test/java/salesTaxes/TaxCalculatorTest.java
@@ -5,54 +5,85 @@ import static org.junit.Assert.*;
 import java.util.Arrays;
 import java.util.Collection;
 
+import org.apache.commons.lang3.StringUtils;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 
 import com.russo.fulvio.bravofly.salestaxes.TaxCalculator;
-
+import com.russo.fulvio.bravofly.salestaxes.utils.AppConfigurationReader;
+import com.russo.fulvio.bravofly.salestaxes.utils.NumberFormatterUtils;
 
 
 @RunWith(Parameterized.class)
 public class TaxCalculatorTest {
 	
-	private double goodPrice;
-	private double priceWithTax;
+	private static final AppConfigurationReader APPLICATION_CONFIG_READER = AppConfigurationReader.getInstance();
 	
-	/* parameters provider per i test cases: goodPrice,priceWithTax  */
+	/* the good name (eg: music CD, imported chocolate, ...) */
+	private String goodName;
+	
+	/* good price without taxes */
+	private String netPrice;
+	
+	/* the type of the good (ie: book, medical...) */
+	private String goodType;
+	
+	/* good price with taxes */
+	private String taxedPrice;
+	
+	
+	/* test parameters provider input: <netPrice>, <good-type>, <taxedType> */
 	@Parameterized.Parameters
-	public static Collection<Double[]> getParam() {
-		return Arrays.asList(new Double[][]{ 
-			{14.99, 16.49}
+	public static Collection<String[]> getParam() {
+		return Arrays.asList(new String[][]{ 
+			{"book","12.49", "food","12.49"},
+			{"imported chocolate","10.00", "food","10.50"},
+			{"chocolate","0.85", "food","0.85"}	
+		
 						
 		});
 	}
 	
-	public TaxCalculatorTest(double goodPrice, double priceWithTax) {
-		this.goodPrice = goodPrice;
-		this.priceWithTax = priceWithTax;
+	/**
+	 * Constructor
+	 * @param netPrice good price without tax
+	 * @param taxedPrice good price with tax
+	 */
+	public TaxCalculatorTest(String goodName,String netPrice, String goodType, String taxedPrice) {
+		this.goodName = goodName;
+		this.netPrice = netPrice;
+		this.goodType = goodType;
+		this.taxedPrice = taxedPrice;
 	}
 
 
 	@Test
-	public void testCalculatePriceWithBasicSalesTax() {
+	public void testPriceWithBasicSalesTax() {
 		
 		TaxCalculator taxCalculator = new TaxCalculator();
 		
-		double taxedValue = taxCalculator.getBasicSalesTax(goodPrice);	
+		double importDutyTax = 0.0;
+        double basicSalesTax = 0.0;
 		
-		assertTrue(priceWithTax==taxedValue);	
-	}
-	
-	
-	@Test
-	public void testCalculatePriceImportDutyTax() {
+        /* check if apply 'basic tax' */
+		if(! StringUtils.containsIgnoreCase(APPLICATION_CONFIG_READER.getBasicSalesTaxExcludedGoods(), goodType)) {	    	            	
+            basicSalesTax = taxCalculator.getBasicSalesTax(Double.parseDouble(netPrice));             
+ 	    } 		
 		
-		TaxCalculator taxCalculator = new TaxCalculator();
+		assertNotNull(basicSalesTax);
 		
-		double taxValue = taxCalculator.getBasicSalesTax(goodPrice);		
+		/* check if apply 'imported duty tax' */
+        if(StringUtils.containsIgnoreCase(goodName, "imported")) {	    	            	
+	       	importDutyTax = taxCalculator.getImportDutyTax(Double.parseDouble(netPrice)); 	       
+        }
 		
-		assertTrue(10.50==taxValue);	
-	}
+		double goodPriceWithTax = importDutyTax + basicSalesTax + Double.parseDouble(netPrice);		
+		
+		
+		assertTrue(Double.parseDouble(taxedPrice)==goodPriceWithTax);	
+	}	
+  		
+} 
 
-}
+
